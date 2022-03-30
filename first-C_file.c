da64b289dca165fd9e8a7a750cab6b9471b7f4e6
Wambu <wggachora@gmail.com>
2022-03-29 19:43:53 -0700

    Finally Understand

---
+++ first-C_file.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Prints out characters using printf
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+	printf("Neso Academy");
+	return (0);
+}
+
